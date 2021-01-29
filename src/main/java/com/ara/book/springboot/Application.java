2d5efe90683d94f32a57f488827d2b456e9265f9
arajo-hub <joara9566@naver.com>
2021-01-29 15:29:41 +0900

    테스트 코드 개선

--- src/main/java/com/ara/book/springboot/Application.java
+++ src/main/java/com/ara/book/springboot/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
+//@EnableJpaAuditing
 // 스프링 부트의 자동 설정, 스프링 Bean 읽기와 생성을 모두 자동으로 설정.
 @SpringBootApplication
 public class Application { // 앞으로 만들 프로젝트의 메인 클래스
--- src/test/java/com/ara/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/ara/book/springboot/web/HelloControllerTest.java
@@ -1,9 +1,13 @@
 package com.ara.book.springboot.web;
 
+import com.ara.book.springboot.config.auth.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import com.ara.book.springboot.web.HelloController;
@@ -15,12 +19,15 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class) // 스프링 부트 테스트와 JUnit 사이의 연결자 역할
-@WebMvcTest(controllers = HelloController.class)
+@WebMvcTest(controllers = HelloController.class, excludeFilters={
+        @ComponentScan.Filter(type= FilterType.ASSIGNABLE_TYPE, classes=SecurityConfig.class)
+})
 public class HelloControllerTest {
 
     @Autowired // 스프링이 관리하는 빈(Bean)을 주입받는다.
     private MockMvc mvc;
 
+    @WithMockUser(roles="USER")
     @Test
     public void hello가_리턴된다() throws Exception {
         String hello="hello";
@@ -30,6 +37,7 @@ public class HelloControllerTest {
                 .andExpect(content().string(hello));
     }
 
+    @WithMockUser(roles="USER")
     @Test
     public void helloDto가_리턴된다() throws Exception {
         String name="hello";
